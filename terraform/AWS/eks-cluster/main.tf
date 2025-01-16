1ea1b490c211ce61e0b1d59a995613d45866b2c0
Ifeoma-Obinna23 <124327032+Ifeoma-Obinna23@users.noreply.github.com>
2025-01-16 23:27:52 +0100

    Update main.tf

--- terraform/AWS/eks-cluster/main.tf
+++ terraform/AWS/eks-cluster/main.tf
@@ -12,7 +12,7 @@ data "aws_iam_policy_document" "assume_role" {
 }
 
 resource "aws_iam_role" "example" {
-  name               = "eks-cluster-cluster"
+  name               = "ifeoma-eks-cluster-cluster"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
